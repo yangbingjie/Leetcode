61b6adcb6cb0f3bbc2845a64c265177a1a501ba8
Bingjie Yang <yangbingjie1997@gmail.com>
2019-10-20 16:15:01 +0800

    155_MinStack improve

--- Easy/155_MinStack/155_MinStack/main.cpp
+++ Easy/155_MinStack/155_MinStack/main.cpp
@@ -7,86 +7,44 @@
 //
 
 #include <iostream>
+#include <stack>
 using namespace std;
-struct MyListNode {
-    int val;
-    MyListNode* smaller;
-    MyListNode* larger;
-    MyListNode* next;
-    MyListNode* last;
-    MyListNode(int x):val(x), smaller(NULL),larger(NULL), next(NULL), last(NULL){}
-};
 class MinStack {
 private:
-    MyListNode* minNode;
-    MyListNode* topNode;
+    stack<int> sta;
 public:
     MinStack() {
-        this->minNode = NULL;
-        this->topNode = NULL;
+        
     }
     
     void push(int x) {
-        MyListNode* node = new MyListNode(x);
-        if (!this->minNode) {
-            this->minNode = node;
-            this->topNode = node;
-            return;
-        }
-        this->topNode->next = node;
-        node->last = this->topNode;
-        this->topNode = node;
-        
-        MyListNode* pos = this->minNode;
-        while (pos->larger && pos->val <= node->val) {
-            pos = pos->larger;
-        }
-               
-        if (pos->larger && pos->smaller) {
-            MyListNode* tmp = pos->smaller;
-            pos->smaller = node;
-            node->smaller = tmp;
-            node->larger = pos;
-            tmp->larger = node;
-        }else if(pos == this->minNode && pos->val >= node->val){
-            this->minNode = node;
-            pos->smaller = node;
-            node->larger = pos;
-        }else if (!pos->larger){
-            pos->larger = node;
-            node->smaller = pos;
+        if (this->sta.empty()) {
+            this->sta.push(x);
         }else{
-            cout << "ERROR" << endl;
+            int tmp = this->sta.top();
+            this->sta.pop();
+            int last_min = this->sta.top();
+            this->sta.push(tmp);
+            this->sta.push(min(last_min, x));
         }
+        this->sta.push(x);
     }
     
     void pop() {
-        MyListNode* node = this->topNode;
-        this->topNode = node->last;
-        if (node->last) {
-            node->last->next = NULL;
-        }
-        if (node->larger && node->smaller) {
-            node->larger->smaller = node->smaller;
-            node->smaller->larger = node->larger;
-        }else if (!node->larger&& node->smaller){
-            node->smaller->larger = NULL;
-        }else if(node->larger && !node->smaller){
-            node->larger->smaller = NULL;
-            this->minNode = node->larger;
-        }else{
-            this->minNode = NULL;
-            this->topNode = NULL;
-        }
-        delete node;
+        this->sta.pop();
+        this->sta.pop();
     }
     
     int top() {
-        return this->topNode->val;
+        return this->sta.top();
     }
     
     int getMin() {
-        return this->minNode->val;
+        int tmp = this->sta.top();
+        this->sta.pop();
+        int last_min = this->sta.top();
+        this->sta.push(tmp);
+        return last_min;
     }
 };
 
