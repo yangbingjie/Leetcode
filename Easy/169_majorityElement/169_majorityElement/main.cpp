d448b705cf5d4ce44e87b1980e07d6d57b699cb5
Bingjie Yang <yangbingjie1997@gmail.com>
2019-10-21 11:52:04 +0800

    169_majorityElement -1

--- Easy/169_majorityElement/169_majorityElement/main.cpp
+++ Easy/169_majorityElement/169_majorityElement/main.cpp
@@ -14,26 +14,14 @@ using namespace std;
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> m;
-        map<int, int>::iterator iter;
-        for (int i = 0; i < nums.size(); ++i) {
-            m[nums[i]]++;
-        }
-        int max = 0;
-        int result = 0;
-        for (iter = m.begin(); iter != m.end(); iter++) {
-            if (iter->second > max) {
-                max = iter->second;
-                result = iter->first;
-            }
-        }
-        return result;
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
     }
 };
 
 int main(int argc, const char * argv[]) {
-    const int LEN = 7;
-    int array[LEN] = {2,2,1,1,1,2,2};
+    const int LEN = 5;
+    int array[LEN] = {2,2,1,1,2};
     Solution s;
     vector<int>nums(array, array + LEN);
     cout << s.majorityElement(nums) << endl;
